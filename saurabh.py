1f12fd7f74578862d58c177c4c7a5f96bfb5dd2f
saurabhsrikant319 <srikant.saurabh319@gmail.com>
2025-04-12 22:34:59 +0530

    Add files via upload

--- saurabh.py
+++ saurabh.py
@@ -4,6 +4,7 @@
 # for i in range(1,len(b)+1):
 #     c+=(b[-i]+" ")
 # print(c)
+from os.path import split
 
 # a=input("string name:")
 # b=a.split()
@@ -11,3 +12,18 @@
 # for i in range(len(b)):
 #     c+=(b[i])
 # print(c)
+
+# a=input("string name:")
+# b=a.split()
+# c=""
+# for i in b:
+#     if len(i)%2==0:
+#         c+=(i+" ")
+# print(c)
+a=input("string name:")
+b=a.split()
+c=""
+for i in b:
+    if len(i)%2!=0:
+        c+=(i+" ")
+print(c)
