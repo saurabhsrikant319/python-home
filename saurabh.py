a6e97a7abd23a3737e04829cc220b762a4058194
saurabhsrikant319 <srikant.saurabh319@gmail.com>
2025-04-12 22:22:03 +0530

    Add files via upload

---
+++ saurabh.py
@@ -0,0 +1,13 @@
+# a=input("string name:")
+# b=a.split()
+# c=""
+# for i in range(1,len(b)+1):
+#     c+=(b[-i]+" ")
+# print(c)
+
+# a=input("string name:")
+# b=a.split()
+# c=""
+# for i in range(len(b)):
+#     c+=(b[i])
+# print(c)
